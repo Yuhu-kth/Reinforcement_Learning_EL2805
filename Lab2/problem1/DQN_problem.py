bc3f6abef5e1d564edc1d7b08a311c1956560cb7
Acander <adrianz@kth.se>
2020-12-14 22:16:14 +0100

    stuff

--- Lab2/problem1/DQN_problem.py
+++ Lab2/problem1/DQN_problem.py
@@ -20,6 +20,7 @@ import gym
 import torch
 import torch.nn.functional as functional
 import torch.nn as nn
+import copy
 from tqdm import trange
 from Lab2.problem1.ExperienceReplayBuffer import ExperienceReplayBuffer, Experience
 from Lab2.problem1.DQN_agent import RandomAgent
@@ -35,15 +36,16 @@ N_ACTIONS = 4  # Number of available actions
 DIM_STATES = 8  # State dimensionality
 
 #  Hyper parameters
-DISCOUNT = 0.1
-BUFFER_SIZE = 5000  # Should be 5000-30000
-BUFFER_EXP_START = 5
+DISCOUNT = 0.05
+BUFFER_SIZE = 10000  # Should be 5000-30000
+BUFFER_EXP_START = 1000
 N_EPISODES = 1000  # Should be 100-1000
 Z = N_EPISODES * 0.95  # Z is usually 90 − 95% of the total number of episodes
 BATCH_SIZE_N = 4  # Should 4-128
 C = BUFFER_SIZE / BATCH_SIZE_N  # Update frequency of the target neural network
-DECAY_MAX = 0.99
+DECAY_MAX = 0.5
 DECAY_MIN = 0.05
+EPS_LINEAR = True
 
 # Hyper parameters, Neural Network
 LEARNING_RATE = 10e-4  # Should be between 10e-3 and 10e-4
@@ -51,21 +53,33 @@ CLIPPING_VALUE = 0.5  # 0.5 and 2
 
 # Training Procedure
 N_EP_RUNNING_AVERAGE = 50
-EARLY_STOPPING_THRESHOLD = -30
+EARLY_STOPPING_THRESHOLD = 200
+
 
 ##############################################
+
+
+def main():
+    model_url = 'neural-network-1.pth'
+
+    # Random agent initialization
+    # agent = RandomAgent(N_ACTIONS)
+
+    # agent = DQNAgentHidden1(DIM_STATES, N_ACTIONS)
+    # target = DQNAgentHidden1(DIM_STATES, N_ACTIONS)
+    agent = DQNAgentHidden2(DIM_STATES, N_ACTIONS)
+    target = DQNAgentHidden2(DIM_STATES, N_ACTIONS)
+    training_DQN(agent, target, model_url)
+
+    # load_model(agent, model_url)
+
+    # trial_run(agent, random=False)
 
 
 def trial_run(agent, random=False):
     # Import and initialize the discrete Lunar Laner Environment
     env = gym.make('LunarLander-v2')
     env.reset()
-
-    # Parameters
-    discount_factor = 0.95  # Value of the discount factor
-    N_EP_RUNNING_AVERAGE = 50  # Running average of 50 episodes
-    n_actions = env.action_space.n  # Number of available actions
-    dim_state = len(env.observation_space.high)  # State dimensionality
 
     # We will use these variables to compute the average episodic reward and
     # the average number of steps per episode
@@ -130,7 +144,7 @@ def trial_run(agent, random=False):
                                 N_EP_RUNNING_AVERAGE)
 
 
-def training_DQN(agent, target, URL):
+def training_DQN(main_network, target_network, URL):
     # Import and initialize the discrete Lunar Laner Environment
     env = gym.make('LunarLander-v2')
     env.reset()
@@ -142,10 +156,8 @@ def training_DQN(agent, target, URL):
 
     buffer = init_buffer()
 
-    # agent = DQNAgentHidden1(DIM_STATES, N_ACTIONS)
-    # target = DQNAgentHidden1(DIM_STATES, N_ACTIONS)
-
-    optimizer = torch.optim.Adam(agent.parameters(), lr=LEARNING_RATE)
+    # optimizer = torch.optim.SGD(main_network.parameters(), lr=LEARNING_RATE)
+    optimizer = torch.optim.Adam(main_network.parameters(), lr=LEARNING_RATE)
 
     ### Training process
 
@@ -154,7 +166,8 @@ def training_DQN(agent, target, URL):
     EPISODES = trange(N_EPISODES, desc='Episode: ', leave=True)
 
     n_episodes_so_far = 0
-    running_average_reward = int("inf")
+
+    c = 0
 
     for k in EPISODES:
         # Reset environment data and initialize variables
@@ -166,7 +179,7 @@ def training_DQN(agent, target, URL):
         while not done:
             # env.render()
 
-            next_state, reward, done = q_step(k, agent, state, env, buffer, target, optimizer, t)
+            next_state, reward, done, target_network = q_step(k, main_network, state, env, buffer, target_network, optimizer, c)
 
             # Update episode reward
             total_episode_reward += reward
@@ -174,6 +187,7 @@ def training_DQN(agent, target, URL):
             # Update state for next iteration
             state = next_state
             t += 1
+            c += 1
 
         # Append episode reward and total number of steps
         episode_reward_list.append(total_episode_reward)
@@ -185,27 +199,22 @@ def training_DQN(agent, target, URL):
         # Updates the tqdm update bar with fresh information
         # (episode number, total reward of the last episode, total number of Steps
         # of the last episode, average reward, average number of steps)
-        running_average_reward_new = Utils.running_average(episode_reward_list, N_EP_RUNNING_AVERAGE)[-1]
+        running_average_reward = Utils.running_average(episode_reward_list, N_EP_RUNNING_AVERAGE)[-1]
         EPISODES.set_description(
             "Episode {} - Reward/Steps: {:.1f}/{} - Avg. Reward/Steps: {:.1f}/{}".format(
                 k, total_episode_reward, t,
-                Utils.running_average(running_average_reward_new, N_EP_RUNNING_AVERAGE)[-1],
+                running_average_reward,
                 Utils.running_average(episode_number_of_steps, N_EP_RUNNING_AVERAGE)[-1]))
 
         # Early stopping
-        n_episodes_so_far = k
-        average_reward_diff = running_average_reward_new - running_average_reward
-        if average_reward_diff < EARLY_STOPPING_THRESHOLD:
+        n_episodes_so_far = k + 1
+        if running_average_reward > EARLY_STOPPING_THRESHOLD:
             break
 
     Utils.plot_reward_and_steps(n_episodes_so_far, episode_reward_list, episode_number_of_steps, Utils.running_average,
                                 N_EP_RUNNING_AVERAGE)
 
-    save_model(agent, URL)
-
-    # return agent
-
-
+    save_model(main_network, URL)
 
 
 def save_model(model, URL):
@@ -217,8 +226,10 @@ def load_model(model, URL):
     model.load_state_dict(torch.load(URL))
 
 
-def q_step(k, agent, state, env, buffer, target, optimizer, t):
-    action = eps_greedy(k, agent, state)
+def q_step(k, main_network, state, env, buffer, target_network, optimizer, c):
+    optimizer.zero_grad()  # Necessery to reset the gradients since pytorch accumulates them by default
+
+    action = eps_greedy(k, main_network, state)
     next_state, reward, done, _ = env.step(action)
     # Append experience to the buffer
     exp_z = Experience(state, action, reward, next_state, done)
@@ -227,16 +238,18 @@ def q_step(k, agent, state, env, buffer, target, optimizer, t):
     # Utils.print_SARSD(state, action, next_state, reward, done)
 
     states, actions, rewards, next_states, dones = buffer.sample_batch(n=BATCH_SIZE_N)
-    y = target_values_y(rewards, target, next_states, done)
+    y = target_values_y(rewards, target_network, next_states, done)
     y_tensor = torch.tensor(y, requires_grad=False, dtype=torch.float32)
 
-    states_tensor = torch.tensor(states, requires_grad=True, dtype=torch.float32)
+    states_tensor = torch.tensor(states, requires_grad=False, dtype=torch.float32)
     actions_tensor = torch.tensor(actions, requires_grad=False, dtype=torch.int64)
-    predicted_Q_values = agent(states_tensor)
+    predicted_Q_values = main_network(states_tensor)
+
     actions_tensor = torch.unsqueeze(actions_tensor, -1)
     # We only want to perform backprop the q-value for the taken action
     predicted_Q_values_for_taken_actions = torch.gather(predicted_Q_values, -1, actions_tensor)
 
+    # Old way of 'removing' irrelevant action outputs
     '''y = np.array([y, ] * N_ACTIONS).transpose()
 
     actions_mask = np.zeros((BATCH_SIZE_N, N_ACTIONS))
@@ -257,27 +270,30 @@ def q_step(k, agent, state, env, buffer, target, optimizer, t):
     loss.backward()
 
     # Clip gradient norm to 1
-    nn.utils.clip_grad_norm_(agent.parameters(), max_norm=1.)
+    nn.utils.clip_grad_norm_(main_network.parameters(), max_norm=1.)
 
     # Perform backward pass (backpropagation)
     optimizer.step()
 
-    if t % C == 0:
-        target.load_state_dict(agent.state_dict())
+    if c % C == 0 and c != 0:
+        target_network = copy.deepcopy(main_network)
 
-    return next_state, reward, done
+    return next_state, reward, done, target_network
 
 
 def eps_greedy(k, agent, state):
-    eps_k = Utils.decay_linear(DECAY_MIN, DECAY_MAX, k, Z)
+    if EPS_LINEAR:
+        eps_k = Utils.decay_linear(DECAY_MIN, DECAY_MAX, k, Z)
+    else:
+        eps_k = Utils.decay_exp(DECAY_MIN, DECAY_MAX, k, Z)
+
     p = np.random.random()
     if p < eps_k:
         return np.random.choice(N_ACTIONS)
     else:
         state_tensor = torch.tensor(state, requires_grad=False, dtype=torch.float32)
-        # Returns the argmax of the network
         # no_mask = torch.tensor(np.ones(N_ACTIONS), requires_grad=False, dtype=torch.float32)
-        return torch.argmax(agent(state_tensor)).item()
+        return torch.argmax(agent(state_tensor)).item()  # Returns the argmax of the network
 
 
 """
@@ -292,17 +308,17 @@ def target_values_y(rewards, target, next_states, done):
 """
 
 
-def target_values_y(rewards, target, next_states, done):
+def target_values_y(rewards, target_network, next_states, done):
     if not done:
         states_tensor = torch.tensor(next_states, requires_grad=False, dtype=torch.float32)
-        Q_theta_prime = target(states_tensor).max(1)[0].detach().numpy()
+        Q_theta_prime = target_network(states_tensor).max(1)[0].detach().numpy()
         return rewards + DISCOUNT * Q_theta_prime
     else:
         return rewards
 
 
-def create_no_mask_tensor():
-    return torch.tensor(np.ones((BATCH_SIZE_N, N_ACTIONS)), requires_grad=False, dtype=torch.float32)
+# def create_no_mask_tensor():
+#    return torch.tensor(np.ones((BATCH_SIZE_N, N_ACTIONS)), requires_grad=False, dtype=torch.float32)
 
 
 def init_buffer():
@@ -351,17 +367,4 @@ def init_buffer():
 
 
 if __name__ == '__main__':
-    model_url = 'neural-network-1.pth'
-
-    # Random agent initialization
-    # agent = RandomAgent(N_ACTIONS)
-
-    # agent = DQNAgentHidden1(DIM_STATES, N_ACTIONS)
-    # target = DQNAgentHidden1(DIM_STATES, N_ACTIONS)
-    agent = DQNAgentHidden2(DIM_STATES, N_ACTIONS)
-    # target = DQNAgentHidden2(DIM_STATES, N_ACTIONS)
-    # training_DQN(agent, target, model_url)
-
-    load_model(agent, model_url)
-
-    trial_run(agent, random=False)
+    main()
