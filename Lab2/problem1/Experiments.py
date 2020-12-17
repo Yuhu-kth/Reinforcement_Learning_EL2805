b3fb6d01f9146c2abd16b90ed334beee2e49a933
Acander <adrianz@kth.se>
2020-12-17 03:22:29 +0100

    first version DQN done

--- Lab2/problem1/Experiments.py
+++ Lab2/problem1/Experiments.py
@@ -1,9 +1,11 @@
-##### SECTION f Experiments #####
-
+##### SECTION f and g Experiments #####
+import gym
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import Lab2.problem1.DQN_agent as DQN_agent
+import matplotlib.pyplot as plt
+from tqdm import trange
 
 # Load model
 try:
@@ -12,6 +14,69 @@ try:
 except:
     print('File neural-network-1.pth not found!')
     exit(-1)
+
+
+def main_experiment_g():
+    random_rewards = run_game(random=True)
+    best_model_rewards = run_game(random=False)
+    x = np.arange(len(random_rewards))
+    plt.title("Episodic Rewards")
+    plt.xlabel("Episode")
+    plt.ylabel("Reward")
+    plt.plot(x, random_rewards, label="Random Agent")
+    plt.plot(x, best_model_rewards, label="Best Q-Network")
+    plt.legend()
+    plt.show()
+
+
+def run_game(random=False):
+    # Import and initialize Mountain Car Environment
+    env = gym.make('LunarLander-v2')
+    env.reset()
+
+    random_agent = DQN_agent.RandomAgent(4)
+
+    # Parameters
+    N_EPISODES = 50  # Number of episodes to run for trainings
+
+    # Reward
+    episode_reward_list = []  # Used to store episodes reward
+
+    # Simulate episodes
+    print('Checking solution...')
+    EPISODES = trange(N_EPISODES, desc='Episode: ', leave=True)
+    for i in EPISODES:
+        EPISODES.set_description("Episode {}".format(i))
+        # Reset enviroment data
+        done = False
+        state = env.reset()
+        total_episode_reward = 0.
+        while not done:
+            # Get next state and reward.  The done variable
+            # will be True if you reached the goal position,
+            # False otherwise
+
+            if random:
+                action = random_agent.forward(state)
+                next_state, reward, done, _ = env.step(action)
+            else:
+                q_values = model(torch.tensor([state]))
+                _, action = torch.max(q_values, axis=1)
+                next_state, reward, done, _ = env.step(action.item())
+
+            # Update episode reward
+            total_episode_reward += reward
+
+            # Update state for next iteration
+            state = next_state
+
+        # Append episode reward
+        episode_reward_list.append(total_episode_reward)
+
+        # Close environment
+        env.close()
+
+    return episode_reward_list
 
 
 def main_experiment_f():
@@ -80,4 +145,5 @@ def plot_argmax_Q_values(y, w):
 
 
 if __name__ == '__main__':
-    main_experiment_f()
+    #main_experiment_f()
+    main_experiment_g()
